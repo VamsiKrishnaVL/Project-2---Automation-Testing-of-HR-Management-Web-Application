bdc8115857cf89268efd2864d15e30b5a67b3a40
VamsiKrishnaVL <vamsikrishna9686@gmail.com>
2026-08-12 22:25:44 +0530

    Delete Project-2 directory

--- Project-2/test_tc02_home_access.py
+++
@@ -1,6 +0,0 @@
-from pages.login_page import LoginPage
-
-def test_tc02_home_access(driver):
-    login = LoginPage(driver)
-    login.load()
-    assert "orangehrmlive" in driver.current_url
--- Project-2/test_tc03_login_fields.py
+++
@@ -1,7 +0,0 @@
-from pages.login_page import LoginPage
-
-def test_tc03_login_fields_presence(driver):
-    login = LoginPage(driver)
-    login.load()
-    assert login.is_visible(LoginPage.USERNAME)
-    assert login.is_visible(LoginPage.PASSWORD)
